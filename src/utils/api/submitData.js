d342a64cd8d126e61a779c79aa1454b579f106bf
Partheeban Bharati <partheeban@Partheebans-MacBook-Air.local>
2023-01-02 16:01:08 -0800

    fix API call for submitData

--- src/utils/api/submitData.js
+++ src/utils/api/submitData.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 import { baseURL } from './baseURL';
 
-export const submitData = async (query) => {
+export const submitData = async (data) => {
     const result = await axios({
         method: 'post',
-        url: `${baseURL}//suggestion`,
-        data: query
+        url: `${baseURL}/submit`,
+        data: data
     });
 
     return result
